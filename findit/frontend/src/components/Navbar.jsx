b9e47d82bb47dacc9e9464cd25236c9a0d4d56f8
Sanchi Namugade <cs.sanchinamugade@gmail.com>
2026-07-26 23:00:28 +0530

    Fixed navbar

--- findit/frontend/src/components/Navbar.jsx
+++ findit/frontend/src/components/Navbar.jsx
@@ -1,310 +1,141 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { FaSearchLocation } from "react-icons/fa";
+import { FaSearchLocation, FaUserCircle } from "react-icons/fa";
 import "./Navbar.css";
 
+function Navbar() {
+  const navigate = useNavigate();
 
-function Navbar(){
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user"))
+  );
 
-    const navigate = useNavigate();
-
-    const [user,setUser] = useState(null);
-
-
-
-    useEffect(()=>{
-
-        const storedUser = localStorage.getItem("user");
-
-
-        if(storedUser){
-
-            setUser(
-                JSON.parse(storedUser)
-            );
-
-        }
-
-
-    },[]);
-
-
-
-
-    const logout = ()=>{
-
-
-        localStorage.removeItem("user");
-
-
-        setUser(null);
-
-
-        navigate("/login");
-
-
+  useEffect(() => {
+    const syncUser = () => {
+      setUser(JSON.parse(localStorage.getItem("user")));
     };
 
-
-
-
-
-    return(
-
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-
-            <div className="container">
-
-
-                {/* LOGO */}
-
-                <Link 
-                className="navbar-brand logo"
-                to="/"
-                >
-
-                    <FaSearchLocation 
-                    className="logo-icon"
-                    />
-
-
-                    <span>
-                        FindIt
-                    </span>
-
-
-                </Link>
-
-
-
-
-
-                {/* Mobile Toggle */}
-
-                <button
-
-                className="navbar-toggler"
-
-                type="button"
-
-                data-bs-toggle="collapse"
-
-                data-bs-target="#navbarMenu"
-
-                >
-
-                    <span className="navbar-toggler-icon"></span>
-
-
-                </button>
-
-
-
-
-
-
-                <div 
-                className="collapse navbar-collapse"
-                id="navbarMenu"
-                >
-
-
-
-                <ul className="navbar-nav ms-auto align-items-center gap-3">
-
-
-
-                    <li className="nav-item">
-
-                        <Link
-                        className="nav-link"
-                        to="/"
-                        >
-
-                            Home
-
-                        </Link>
-
-                    </li>
-
-
-
-
-
-                    <li className="nav-item">
-
-                        <Link
-                        className="nav-link"
-                        to="/search"
-                        >
-
-                            Search
-
-                        </Link>
-
-                    </li>
-
-
-
-
-
-
-                    <li className="nav-item">
-
-                        <Link
-                        className="nav-link"
-                        to="/report-lost"
-                        >
-
-                            Report Lost
-
-                        </Link>
-
-                    </li>
-
-
-
-
-
-
-                    <li className="nav-item">
-
-                        <Link
-                        className="nav-link"
-                        to="/report-found"
-                        >
-
-                            Report Found
-
-                        </Link>
-
-                    </li>
-
-
-
-
-
-
-
-                    {
-
-                    user ?
-
-
-                    <>
-
-
-                    <li>
-
-                        <Link
-
-                        className="btn btn-light profile-btn"
-
-                        to="/profile"
-
-                        >
-
-                            Profile
-
-                        </Link>
-
-
-                    </li>
-
-
-
-
-
-                    <li>
-
-                        <button
-
-                        className="btn btn-danger"
-
-                        onClick={logout}
-
-                        >
-
-                            Logout
-
-                        </button>
-
-
-                    </li>
-
-
-                    </>
-
-
-
-                    :
-
-
-                    <>
-
-
-                    <li>
-
-                        <Link
-
-                        className="btn btn-light"
-
-                        to="/login"
-
-                        >
-
-                            Login
-
-                        </Link>
-
-
-                    </li>
-
-
-
-
-
-                    <li>
-
-                        <Link
-
-                        className="btn btn-warning"
-
-                        to="/register"
-
-                        >
-
-                            Register
-
-                        </Link>
-
-
-                    </li>
-
-
-
-                    </>
-
-
-                    }
-
-
-
-                </ul>
-
-
-                </div>
-
-
-            </div>
-
-
-        </nav>
-
-
-    );
-
+    window.addEventListener("storage", syncUser);
+
+    syncUser();
+
+    return () => {
+      window.removeEventListener("storage", syncUser);
+    };
+  }, []);
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+
+    setUser(null);
+
+    navigate("/", { replace: true });
+
+    window.location.reload();
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <div className="container">
+
+        <Link className="navbar-brand logo" to="/">
+          <FaSearchLocation className="logo-icon" />
+          <span>FindIt</span>
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarMenu"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="collapse navbar-collapse"
+          id="navbarMenu"
+        >
+          <ul className="navbar-nav ms-auto align-items-center gap-3">
+
+            <li>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+
+            <li>
+              <Link className="nav-link" to="/search">
+                Search
+              </Link>
+            </li>
+
+            <li>
+              <Link className="nav-link" to="/report-lost">
+                Report Lost
+              </Link>
+            </li>
+
+            <li>
+              <Link className="nav-link" to="/report-found">
+                Report Found
+              </Link>
+            </li>
+
+            {user ? (
+              <>
+                <li>
+                  <Link className="nav-link" to="/myreports">
+                    My Reports
+                  </Link>
+                </li>
+
+                <li className="user-info">
+                  <FaUserCircle className="user-icon" />
+
+                  <span>
+                    {user.name}
+                  </span>
+                </li>
+
+                <li>
+                  <button
+                    className="btn logout-btn"
+                    onClick={logout}
+                  >
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link
+                    className="btn login-btn"
+                    to="/login"
+                  >
+                    Login
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    className="btn register-btn"
+                    to="/register"
+                  >
+                    Register
+                  </Link>
+                </li>
+              </>
+            )}
+
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+  );
 }
-
 
 export default Navbar;
